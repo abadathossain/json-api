d60b30c98db817d1501639d93ccce075bee25cf0
Abadat Hossain <abadat26@gmail.com>
2024-10-01 09:38:10 +0600

    json api practice done

---
+++ js/api.js
@@ -0,0 +1,22 @@
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+}
+
+function loadUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>displayUser(data))
+}
+
+function displayUser(data){
+const ul=document.getElementById('user-list');
+for(const user of data){
+    const li=document.createElement('li');
+    li.innerText=user.username;
+    ul.appendChild(li)
+}
+   
+}
+
